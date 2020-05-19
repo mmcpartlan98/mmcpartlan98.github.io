ba525008169bb8cd823d3285b5f32f3bc6ad4fe6
Matt McPartlan <35271540+mmcpartlan98@users.noreply.github.com>
2020-05-18 22:35:15 -0700

    Added user control and other goodies

--- mp/script.js
+++ mp/script.js
@@ -1,11 +1,71 @@
-var i;
-for (i = 100; i > 0; i--) {
-  task(i);
+var meltingPoint = 60;
+// Set initial conditions
+var defaultRampStart = 40;
+var defaultRampEnd = 120;
+var defaultRampRate = 40; //Degrees C per min
+
+var rampStart = defaultRampStart;
+var rampEnd = defaultRampEnd;
+var rampRate = defaultRampRate; //Degrees C per min
+var temp = rampStart;
+var timeoutQueue = new Array(setTimeout(function() {
+  console.log("Starting...");
+  document.getElementById("start").innerHTML = "Start temperature: ".concat(String(rampStart.toFixed(2)), " C");
+  document.getElementById("end").innerHTML = "End temperature: ".concat(String(rampEnd.toFixed(2)), " C");
+  document.getElementById("ramp").innerHTML = "Ramp rate: ".concat(String(rampRate.toFixed(2)), " C/min");
+}, 10));
+
+function task2(temp, iterations) {
+  timeoutQueue.push(setTimeout(function() {
+    document.getElementById("thermometer").innerHTML = "Temperature: ".concat(String(temp.toFixed(2)), " C");
+    if (temp > meltingPoint * 0.85){
+      console.log("Melting: ".concat(String(temp)));
+      document.getElementById("endImage").style.opacity = String((temp - meltingPoint * 0.85) / (meltingPoint * 0.20));
+    } else {
+      console.log("Not Melting: ".concat(String(temp)));
+    }
+  }, 100 * iterations));
 }
 
-function task(i) {
-  setTimeout(function() {
-      console.log(document.getElementById("endImage").style.opacity);
-      document.getElementById("endImage").style.opacity = String(i/100);
-  }, 200 * i);
+function startButton() {
+  console.log("Start detected!")
+  // Loop updates 10 times per second
+  var iterations = 0;
+  while (temp < rampEnd) {
+    task2(temp, iterations);
+    iterations++;
+    temp += (rampRate/60)/10;
+  }
+  temp = rampEnd;
+  task2(temp, iterations)
+}
+
+function resetButton() {
+  temp = rampStart;
+  document.getElementById("thermometer").innerHTML = "Temperature: ".concat(String(temp.toFixed(2)), " C");
+  document.getElementById("endImage").style.opacity = "0";
+  rampStart = defaultRampStart;
+  rampEnd = defaultRampEnd;
+  rampRate = defaultRampRate;
+  document.getElementById("rstart").value = document.getElementById("rstart").defaultValue;
+  document.getElementById("rend").value = document.getElementById("rend").defaultValue;
+  document.getElementById("rrate").value = document.getElementById("rrate").defaultValue;
+  document.getElementById("start").innerHTML = "Start temperature: ".concat(String(rampStart.toFixed(2)), " C");
+  document.getElementById("end").innerHTML = "End temperature: ".concat(String(rampEnd.toFixed(2)), " C");
+  document.getElementById("ramp").innerHTML = "Ramp rate: ".concat(String(rampRate.toFixed(2)), " C/min");
+  timeoutQueue.forEach(index => {
+      clearTimeout(index);
+  });
+  console.log("Reset detected!")
+}
+
+function changeButton() {
+  rampStart = parseFloat(document.getElementById("rstart").value);
+  rampEnd = parseFloat(document.getElementById("rend").value);
+  rampRate = parseFloat(document.getElementById("rrate").value);
+  temp = rampStart;
+  document.getElementById("start").innerHTML = "Start temperature: ".concat(String(rampStart.toFixed(2)), " C");
+  document.getElementById("end").innerHTML = "End temperature: ".concat(String(rampEnd.toFixed(2)), " C");
+  document.getElementById("ramp").innerHTML = "Ramp rate: ".concat(String(rampRate.toFixed(2)), " C/min");
+  console.log("Change detected!")
 }
